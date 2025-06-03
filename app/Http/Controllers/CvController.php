b7711f6c2cbe3c85b39b9bb72b3cf75c1f345ae8
obada <opadaalziede@gmail.com>
2025-06-03 18:42:08 +0300

    add caching mechanism

--- app/Http/Controllers/CvController.php
+++ app/Http/Controllers/CvController.php
@@ -3,18 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Storage;
 
 class CvController extends Controller
 {
     public function index(): View
     {
-        return view('cv', ['cvData' => $this->parseCvJsonFile()]);
+        $cvData = Cache::remember('cvData', now()->addDay(), function () {
+            return $this->parseCvJsonFile();
+        });
+
+        return view('cv', ['cvData' => $cvData]);
     }
 
     private function parseCvJsonFile(): array
     {
-        if(Storage::disk('public')->exists('resume.json')){
+        if (Storage::disk('public')->exists('resume.json')) {
             return Storage::disk('public')->json('resume.json');
         }
 
